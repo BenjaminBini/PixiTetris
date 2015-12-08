8478e5174208ee26530a25550f9a9fa84a5694e3
BenjaminBini <benjamin.bini@gmail.com>
2015-12-08 17:59:42 +0100

    Add next tetromino, pausing and some refactoring

--- src/Game.js
+++ src/Game.js
@@ -8,36 +8,42 @@ export default class Game {
     console.log(PIXI);
 
     // DOM container
-    this._domContainer = document.getElementById('game');
+    this._domContainer = document.querySelector(Constants.DOM.CONTAINER);
+
+    // Next tetromino DOM container
+    this._domNextContainer = document.querySelector(Constants.DOM.NEXT)
 
     // Keyboard events
-    var leftKey = this._keyboard(37);
-    var upKey = this._keyboard(38);
-    var rightKey = this._keyboard(39);
-    var downKey = this._keyboard(40);
-    leftKey.press = () => this._pressLeft();
-    upKey.press = () => this._pressUp();
-    rightKey.press = () => this._pressRight();
-    downKey.press = () => this._pressDown();
+    this._initKeyboardEvents();
+
+    // Mouse events
+    this._initMouseEvents();
      
-    // Set up PIXI and launch game
-    this.renderer = PIXI.autoDetectRenderer(Constants.WIDTH * Constants.SQUARE_SIZE, Constants.HEIGHT * Constants.SQUARE_SIZE);
-    //this.renderer.backgroundColor = 0x4CA4B9;
-    this._domContainer.appendChild(this.renderer.view);
+    // Set up PIXI
+    this._renderer = PIXI.autoDetectRenderer(Constants.WIDTH * Constants.SQUARE_SIZE, Constants.HEIGHT * Constants.SQUARE_SIZE);
+
+    this._domContainer.appendChild(this._renderer.view);
 
     // Pixi container
-    this.container = new PIXI.Container();
+    this._container = new PIXI.Container();
 
     // Game board/stage
-    this.stage = new Stage(this.container); 
+    this._stage = new Stage(this._container); 
 
-    // Current moving tetromino
-    this.tetromino = Tetromino.getRandom(this.container);
+    // Init tetrominos
+    this._tetromino = undefined; // Tetromino on the stage
+    this._nextTetromino = undefined; // Next tetromino
+    this._newTetromino();
 
     // Delay between moves
-    this.delay = 300;
+    this._delay = 300;
+
+    // Init timer
+    this._timer = new Date().getTime();
 
     // GO!
+    this._requestId = undefined; // requestAnimationFrame ID (used to pause game)
+    this._paused = false;
     this._start();
   }
 
@@ -45,73 +51,144 @@ export default class Game {
    * Start the game
    */
   _start() {
-    var self = this;
-    var timer = new Date().getTime();
-    function loop() { // Event loop
-      if (new Date().getTime() - timer > self.delay) {
-        timer = new Date().getTime();
-        self.tetromino.move(0, 1); // Gravity
-        // If collision, cancel  move and unite the tetromino with the game stage
-        if (self.stage.isCollision(self.tetromino)) { 
-          self.tetromino.move(0, -1);
-          self.stage.unite(self.tetromino);
-          self.tetromino = Tetromino.getRandom(self.container);
-          // Lose! Restart
-          if (self.stage.isCollision(self.tetromino)) {
-            self.stage.reset();
-          }
-        }
-        self._render(); // Render
+    this._requestId = requestAnimationFrame(() => this._loop());
+  }
+
+  _loop() {
+    if (new Date().getTime() - this._timer > this._delay) {
+      this._timer = new Date().getTime();
+      this._tetromino.move(0, 1); // Gravity
+      // If collision, cancel  move and unite the tetromino with the game stage
+      if (this._stage.isCollision(this._tetromino)) { 
+        this._tetromino.move(0, -1);
+        this._stage.unite(this._tetromino);
+        this._newTetromino();
       }
-      requestAnimationFrame(loop);
+      this._render(); // Render
     }
-    requestAnimationFrame(loop);
+    this._requestId = requestAnimationFrame(() => this._loop());
+  }
+
+  _pause() {
+    this._paused = !this._paused;
+    // Stop or restart loop
+    if (this._paused) {
+      cancelAnimationFrame(this._requestId);
+      document.querySelector(Constants.DOM.START_PAUSE).id = 'start';
+      document.querySelector(Constants.DOM.START_PAUSE).innerText = 'start';
+    } else {
+      this._start();   
+      document.querySelector(Constants.DOM.START_PAUSE).id = 'pause';
+      document.querySelector(Constants.DOM.START_PAUSE).innerText = 'pause';
+    }
+  }
+
+  _newTetromino() {
+    if (!this._nextTetromino) {
+      this._nextTetromino = Tetromino.getRandom(this._container);  
+    }
+    this._tetromino = this._nextTetromino;
+    this._nextTetromino = Tetromino.getRandom(this._container);
+    this._domNextContainer.className = this._nextTetromino.type.name;
+    // Lose! Restart
+    if (this._stage.isCollision(this._tetromino)) {
+      this._stage.reset();
+    }
+  }
+
+  /**
+   * Init keyboard events
+   */
+  _initKeyboardEvents() {
+    var leftKey = this._keyboard(37);
+    var upKey = this._keyboard(38);
+    var rightKey = this._keyboard(39);
+    var downKey = this._keyboard(40);
+    var spaceKey = this._keyboard(32);
+    var pKey = this._keyboard(80);
+    leftKey.press = () => this._pressLeft();
+    upKey.press = () => this._pressUp();
+    rightKey.press = () => this._pressRight();
+    downKey.press = () => this._pressDown();
+    spaceKey.press = () => this._pressSpace();
+    pKey.press = () => this._pause();
+  }
+
+  /**
+   * Init mouse events
+   */
+  _initMouseEvents() {
+    var startPauseButton = document.querySelector(Constants.DOM.START_PAUSE);
+    startPauseButton.addEventListener('click', () => this._pause());
   }
 
   /**
    * "Press left" event
    */
   _pressLeft() {
-    this.tetromino.move(-1, 0);
-    if (this.stage.isCollision(this.tetromino)) {
-      this.tetromino.move(1, 0);
+    if (!this._paused) {
+      this._tetromino.move(-1, 0);
+      if (this._stage.isCollision(this._tetromino)) {
+        this._tetromino.move(1, 0);
+      }
+      this._render();
     }
-    this._render();
   }
 
   /**
    * "Press right" event
    */
   _pressRight() {
-    this.tetromino.move(1, 0);
-    if (this.stage.isCollision(this.tetromino)) {
-      this.tetromino.move(-1, 0);
+    if (!this._paused) {
+      this._tetromino.move(1, 0);
+      if (this._stage.isCollision(this._tetromino)) {
+        this._tetromino.move(-1, 0);
+      }
+      this._render();
     }
-    this._render();
   }
 
   /**
    * "Press up" event
    */
   _pressUp() {
-    this.tetromino.rotate();
-    if (this.stage.isCollision(this.tetromino)) {
-      this.tetromino.antiRotate();
+    if (!this._paused) {
+      this._tetromino.rotate();
+      if (this._stage.isCollision(this._tetromino)) {
+        this._tetromino.antiRotate();
+      }
+      this._render();
     }
-    this._render();
   }
 
   /**
    * "Press down" event
    */
   _pressDown() {
-    this.tetromino.move(0, 1);
-    if (this.stage.isCollision(this.tetromino)) {
-      this.tetromino.move(0, -1);
-      this.stage.unite(this.tetromino);
-      this.tetromino = Tetromino.getRandom(this.container); 
+    if (!this._paused) {
+      this._tetromino.move(0, 1);
+      if (this._stage.isCollision(this._tetromino)) {
+        this._tetromino.move(0, -1);
+        this._stage.unite(this._tetromino);
+        this._newTetromino();
+      }
+      this._render();
     }
-    this._render();
+  }
+
+  /**
+   * "Press space" event
+   */
+  _pressSpace() {
+    if (!this._paused) {
+      while (!this._stage.isCollision(this._tetromino)) {
+        this._tetromino.move(0, 1);
+      }
+      this._tetromino.move(0, -1);
+      this._stage.unite(this._tetromino);
+      this._newTetromino();
+      this._render();
+    }
   }
 
   /**
@@ -120,10 +197,10 @@ export default class Game {
   _render() {
     // Remove everything from the container, redraw stage and tetromino and render
     // TODO : do not remove and redraw at every move
-    this.container.removeChildren();
-    this.stage.draw();
-    this.tetromino.draw();
-    this.renderer.render(this.container);
+    this._container.removeChildren();
+    this._stage.draw();
+    this._tetromino.draw();
+    this._renderer.render(this._container);
   }
 
   /**
